72f0a1a631143871614b7ef561f3f81bbad329e7
geodaio <148104450+geodaio@users.noreply.github.com>
2023-10-19 11:36:38 -0400

    Update favicon.js
    -Added function

--- favicon.js
+++ favicon.js
@@ -1,17 +1,20 @@
-const favicon = document.getElementById("favicon");
-const mode = window.matchMedia("(prefers-color-scheme: dark)");
+function changeFavicon () {
+  const favicon = document.getElementById("favicon");
+  const mode = window.matchMedia("(prefers-color-scheme: dark)");
+  
+  console.log(favicon);
+  console.log(mode);
+  
+  mode.addEventListener("change", (event) => {
+    if (event.matches) {
+      favicon.setAttribute("href", "favicon-dark.io");
+      console.log("darkmode");
+    }
+    else {
+      favicon.setAttribute("href", "favicon-light.io");
+      console.log("lightmode");
+    }
+  });
+}
 
-console.log(favicon);
-console.log(mode);
-
-mode.addEventListener("change", (event) => {
-  if (event.matches) {
-    favicon.setAttribute("href", "favicon-dark.io");
-    console.log("darkmode");
-  }
-  else {
-    favicon.setAttribute("href", "favicon-light.io");
-    console.log("lightmode");
-  }
-});
 
