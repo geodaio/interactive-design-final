a2ac11c0d798749d5bbe235f717e2bb9141c3b6d
geodaio <148104450+geodaio@users.noreply.github.com>
2023-10-24 09:19:02 -0400

    Update logInOut.js
    -cooking

--- logInOut.js
+++ logInOut.js
@@ -4,7 +4,22 @@ function storeCookies (name, value){
 }
 document.getElementById("sign-in").onclick = function() {
   storeCookies("loggedIn", "true");
+  window.location = "https://geodaio.github.io/interactive-design-final/index-login";
 }
 document.getElementById("sign-out").onclick = function() {
   storeCookies("loggedIn", "false");
+  window.location = "https://geodaio.github.io/interactive-design-final";
+}
+onload = function checkSign(){
+  var allCookies = document.cookie;
+
+  var splitCookies = allCookies.split(";";
+
+  for (var c = 0; c<spilitCookies.length; c++){
+    cleanCookie = splitCookies[c].trim();
+    cleanerCookie = cleanCookie.split("=");
+    if (cleanerCookie.equalsIgnoreCase("true")) {
+        window.location = "https://geodaio.github.io/interactive-design-final/index-login";
+    }
+  }
 }
