edfafd8e8c1e42517043743015656cf970c9472e
geodaio <148104450+geodaio@users.noreply.github.com>
2023-10-24 21:37:25 -0400

    Update home.js
    -test

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
 function changeLang(id) {
   event.preventDefault();
-  document.getElementById("lang").innerHTML = id;
+  document.getElementById("lang").innerHTML = "\<img src=\"Globe_icon.svg\" alt=\"ALTTEXT\" id=\"dropdown-img\"\>"+id;
 }
