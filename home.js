774044c19ab98cd2b95915b8a1512d023e668b2a
geodaio <148104450+geodaio@users.noreply.github.com>
2023-10-31 00:49:24 -0400

    Update home.js
    -typo LMAO

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-onload = getElementById("body").style.display = "block";
+onload = document.getElementById("body").style.display = "block";
 function changeLang(id) {
   event.preventDefault();
   document.getElementById("lang").innerHTML = "\<img src=\"Globe_icon.svg\" alt=\"ALTTEXT\" id=\"dropdown-img\"\>"+id;
