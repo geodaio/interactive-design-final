5aee7a52549dd8d499d0aa329c38830cd36d558f
geodaio <148104450+geodaio@users.noreply.github.com>
2023-11-01 19:12:34 -0400

    Update and rename profile.html to profile.php
    -lets see if this works

--- profile.php
+++ profile.php
@@ -13,42 +13,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&family=PT+Serif&display=swap" rel="stylesheet"> <!--Previous three links are links for Google Fonts-->
+	<?php include ("nav-login.php"); ?>
 </head>
     <body id="body">
-                <header id="header-in">
-            <nav class="flexbox">
-                <div class="header-left">
-                    <img src="SNLogo.svg" alt="Logo Temp" id="header-logo">
-                    <a href="https://geodaio.github.io/interactive-design-final/" title="Temp">Home</a>
-                    <a href="hub.html" title="Temp">Study Hub</a>
-                    <a href="" title="Temp">Communities</a>
-                    <a href="" title="Temp">About Us</a>
-                </div>
-                <div class="header-right">
-                    <div class="dropdown">
-                        <button class="dropdown-button" id="lang"><img src="Globe_icon.svg" alt="ALTTEXT" id="dropdown-img">English</button>
-                        <div class="lang-options">
-                            <a href="" title="English" id="English" onclick="changeLang(this.id)">English</a>
-                            <a href="" title="Spanish" id="Spanish" onclick="changeLang(this.id)">Spanish</a>
-                            <a href="" title="French" id="French" onclick="changeLang(this.id)">French</a>
-                            <a href="" title="German" id="German" onclick="changeLang(this.id)">German</a>
-                            <a href="" title="Russian" id="Russian" onclick="changeLang(this.id)">Russian</a>
-                        </div>
-                    </div>
-                    <button class="notification-bell" id="notifications-header"><img src="notification_logo.svg" alt="ALTTEXT" id="notification-bell-img"></button>
-                    <div class="dropdown">
-                        <button class="dropdown-button" id="profile-header"><img src="profile.jpeg" alt="ALTTEXT" id="profile-pic-header"></button>
-                        <div class="lang-options">
-                            <a href="profile.html" title="" id="profile-drop-header">Profile</a>
-                            <a href="messages.html" title="" id="messages-header">Messages</a>
-                            <a href="" title="" id="settings-header" onclick="changeLang(this.id)">Settings</a>
-                            <a href="" title="" id="log-out-header" onclick="signOut()">Log Out</a>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </header>
-
         <section id="profile-sect-1">
             <div class="flexbox-column">
                 <img src="blurred-background-coffee-shop-garden-blur-background-with-bokeh-vintage-filtered-image.jpg" alt="" title="" id="profile-header-img-profile">
